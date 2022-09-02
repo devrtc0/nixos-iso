0b170ef1dc66cb9f0f7d497b3015cc993a7db3fe
Azat Mirzaianov <me@devrtc0.ru>
2022-09-02 16:52:18 +0400

    Update build.1.sh

--- build.1.sh
+++ build.1.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-nix build .#nixosConfigurations.nixosIso.config.system.build.isoImage
+nix build .#nixosConfigurations.nixosIso.config.system.build.isoImage --extra-experimental-features nix-command --extra-experimental-features flakes
