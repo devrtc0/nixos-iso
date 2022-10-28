fae0ca478f6073df3d52a275843162d1bbb45e13
devrtc0 <azat.mirzaianov@gmail.com>
2022-10-28 21:26:21 +0400

    update

--- build.1.sh
+++ build.1.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
+# nix flake update
+
 nix build .#nixosConfigurations.nixosIso.config.system.build.isoImage --extra-experimental-features nix-command --extra-experimental-features flakes
